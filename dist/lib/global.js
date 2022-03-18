92c5371e68f4b234fdee8a7179a609c64194e8f8
Herlangga Sefani <herlangga.sefani@gmail.com>
2022-03-18 10:55:12 +0700

    Circle CI Test

--- dist/lib/global.js
+++ dist/lib/global.js
@@ -3,7 +3,7 @@
  * @author : Herlangga Sefani <https://github.com/gaibz>
  */
 function toQueryString(data) {
-    return Object.keys(data).map(function (key) { return key + "=" + encodeURIComponent(data[key]); }).join('&');
+    return Object.keys(data).map(function (key) { return "".concat(key, "=").concat(encodeURIComponent(data[key])); }).join('&');
 }
 module.exports = { toQueryString: toQueryString };
 //# sourceMappingURL=global.js.map
